7e9f450e9dc45f58512d7b6a30dc6a878395f34f
Enes Yavuz <ensyvz14@gmail.com>
2022-02-12 12:00:19 +0100

    Fixed vulnerability

--- formsite/settings.py
+++ formsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d&#0ui=gue6f&f1j0ld@o)337xkf3an4m5p=*a94ac@=$mhhg@'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
